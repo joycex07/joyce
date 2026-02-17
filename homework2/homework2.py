9f76c5d2c98f75a6f12fa1359c365802b3b98d44
Joyce Xu <joycexu@wifi-10-43-70-76.wifi.berkeley.edu>
2026-02-17 15:18:30 -0800

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,78 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+# Git is used to track changes to files and code.
+# GitHub is a host that allows you to store the proects in the cloud and collaborate with others on projects.
+# Git Bash is a command-line interface for Git on Windows. 
+
+# 2) What’s the difference between the terminal and the command line?
+# Terminal is a window or program that allows you to interact with your computer using text commands.
+# Command line is the interface inside the terminal that processes commands, where it interprets and runs the commands you type.
+
+# 3) How does Windows PowerShell differ from Git Bash?
+# Windows PowerShell is a Microsoft shell and scripting environment where it runs commands to control your computer.
+# Git Bash differs from PowerShell because it is a terminal for Windowns that runs Unix/Linux commands that Windows normally doesn't have.
+
+# 4) What’s the difference between Anaconda, conda, and Python?
+# Python is a programming langauge.
+# Conda is a package manager and environment manager, allowing you to install packages and create isolated environments.
+# Anaconda is a distribution of Python that comes iwth Python, the Conda package, and many other pre-installed libraries.
+
+# 5) What is VS Code? 
+# VS Code is a code editor by Microsoft that allows you to write and run code.
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+# Jupyter Notebook is a web-based interactive environment for writig and running Python code, whereas JupyterLab is more flexible, offering a multi-tab, multi-panel workspace.
+
+# 7) What does ~/ mean?
+# ~/ is the path relative to the home directory.
+
+# 8) What’s the difference between an absolute path and a relative path?
+# An absolute path starts from the root of the file system and gives the full address to a file or folder.
+# A relative path is relative to your current directory. 
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+# absolute: /Users/joycexu/python_decal_sp26/course_assignments/homework2
+# relative: cd ..
+#           couse_assignmnets/homework2
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+# cd ..
+
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+# It would remove/delete the curent directory
+
+# 12) What do the following commands do?
+# git add: stages changes in your working directory so they're ready to be saved in the next commit
+# git commit: saves the staged changes permanently in your local Git repository.
+# git push: connects your local commits to the remote repository (GitHub).
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+# git add . stages all changes in the current directory, while git add <file> stages changes in only the specific file.
+
+# 14) What do "git status" and "git log -1" do?
+# git status shows the current state of your working directory.
+# git log -1 shows the most recent commit in your repository.
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+# Cloning a repository (git clone) creates a full copy of a remote repository on your local computer.
+# Pulling from a repository (git pull) updates your local repository with the latest changes from the remote repository, where you essentially sync it with other peoples' changes.
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+# My most frustrating error so far was when I couldn't find the SSH key. Eventually, I realized I didn't set one up, so I had to generate one and upload it to GitHub.
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+# NA
+
+# 18) Tell me a fun fact!
+# I have two cats
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print(2026//10%10)
+# This expression takes the second to last digit of 2026, which is 2.
